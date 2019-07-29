660b7345cbd6d6edae9f8a9f0cfe5b197580561a
SachaGit <alex.antoine.caron@hotmail.com>
2019-07-29 04:13:49 -0400

    Working on new convergence test

--- Gauss_Jaccobi/Source.cpp
+++ Gauss_Jaccobi/Source.cpp
@@ -32,7 +32,7 @@ SOFTWARE.*/
 int main() {
 	
 	//exemple float
-	int size = 100; // For a square grid [size * size]
+	int size = 32; // For a square grid [size * size]
 	float * laplacian = (float*) malloc(size *size *size *size * sizeof(float)); 
 
 	laplacian = laplacian2d_matrix(size*size, size);
@@ -52,8 +52,9 @@ int main() {
 	
 	//show_vector(vector, size*size);
 	//test_laplacian = jacobi((float*)laplacian, vector, size*size,1e-4F);
-	//test_laplacian = sidel((float*)laplacian, vector, size*size, 1e-4F);
-	test_laplacian = sor((float*)laplacian, vector, size*size, 1e-4F);
+	//test_laplacian = old((float*)laplacian, vector, size*size, 1e-4F);
+	test_laplacian = sidel((float*)laplacian, vector, size*size, 1e-4F);
+	//test_laplacian = sor((float*)laplacian, vector, size*size, 1e-4F);
 	//test_laplacian = v_cylce((float*)laplacian, vector, size*size, 1e-4F);
 
 	//Export Data to a csv file
--- Gauss_Jaccobi/sidel_gauss.cpp
+++ Gauss_Jaccobi/sidel_gauss.cpp
@@ -2,9 +2,46 @@
 #include <iostream>
 float * sidel(float * matrix, float * vector, int r, float precision) {
 	float * x = new float[r];
-	float * x_holder = new float[r];
+	float error = precision + 1;
+	float residual;
+	int count = 0;
+	while(10000  >  count){
+		error = 0;
+		for (int n = 0; n < r; n++) {
+			residual = 0;
+			x[n] = 0;
+			x[n] = vector[n] / (*(matrix + n * r + n));
+			for (int i = 0; i < r; i++) {
+				if (n != i) {
+					x[n] -= (*(matrix + n * r + i)*x[i]) / (*(matrix + n * r + n));
+				}
+			}
+
+			if (count != 0) {
+				for (int i = 0; i < r; i++) {
+					residual += *(matrix + n * r + i) * x[i];
+				}
+				residual -= vector[n];
+				if (residual > error)
+					error = residual;
+			}
+			else {
+				error=  precision + 1;
+			}
+
+		}
+		
+		std::cout << count << std::endl;
+		count++; 
+	}
+	return x;
+}
+
+double * sidel(double * matrix, double * vector, int r, float precision) {
+	double * x = new double[r];
+	double * x_holder = new double[r];
 	bool convergence = true;
-	while(convergence == true){
+	while (convergence == true) {
 		convergence = true;
 
 		//actual solver
@@ -30,9 +67,10 @@ float * sidel(float * matrix, float * vector, int r, float precision) {
 	return x;
 }
 
-double * sidel(double * matrix, double * vector, int r, float precision) {
-	double * x = new double[r];
-	double * x_holder = new double[r];
+
+float * old(float * matrix, float * vector, int r, float precision) {
+	float * x = new float[r];
+	float * x_holder = new float[r];
 	bool convergence = true;
 	while (convergence == true) {
 		convergence = true;
